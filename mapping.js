6db21409e2404da1f2efd0b137f4fd6b7ed103e5
ParaN3xus <paran3xus007@gmail.com>
2024-07-07 16:54:03 +0800

    feat: mapping.js

---
+++ mapping.js
@@ -0,0 +1,15 @@
+
+const fontMapping = {
+    "mathrm": "upright",
+    "mbox": "upright",
+    "hbox": "upright",
+    "mathbf": "mitexmathbf",
+    "mathit": "italic",
+    "mathsf": "sans",
+    "mathtt": "mono",
+    "mathbb": "bb",
+    "mathcal": "cal",
+    "mathfrak": "frak"
+};
+
+module.exports = fontMapping;
