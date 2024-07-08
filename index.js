aaefbef0627eb44b8143cf1c414d61415f2edfa3
ParaN3xus <paran3xus007@gmail.com>
2024-07-08 22:08:31 +0800

    feat: process lines & preprocess

--- index.js
+++ index.js
@@ -1,19 +1,67 @@
 import katex from 'katex';
 import fs from 'fs';
 import path from 'path';
+import readline from 'readline';
 import { build_expression } from './build.js';
 import { fileURLToPath } from 'url';
 
+function preprocess(exp) {
+    if (exp[0] == "%") {
+        exp = exp.substr(1, exp.length - 1);
+    }
+    exp = exp.split('%')[0];
 
-function convert(expression) {
-    var tree = katex.__parse(expression, {});
+    exp = exp.split('\\~').join(' ');
 
-    var typ_expression = build_expression(tree)
-
-    return typ_expression;
+    while (exp.indexOf("\\>") !== -1 || exp.indexOf("$") !== -1 || /\\label{.*?}/.test(exp)) {
+        exp = exp.replace(/\\>/, " ");
+        exp = exp.replace('$', ' ');
+        exp = exp.replace(/\\label{.*?}/, "");
+    }
+    
+    if (exp.indexOf("matrix") == -1 && exp.indexOf("cases") == -1 &&
+        exp.indexOf("array") == -1 && exp.indexOf("begin") == -1) {
+        while (exp.indexOf("\\\\") !== -1) {
+            exp = exp.replace(/\\\\/, "\\,");
+        }
+    }
+    
+    while (exp.indexOf("{\\rm") !== -1 || exp.indexOf("{ \\rm") !== -1 || exp.indexOf("\\rm{") !== -1) {
+        exp = exp.replace(/{\\rm/, "\\mathrm{");
+        exp = exp.replace(/{ \\rm/, "\\mathrm{");
+        exp = exp.replace(/\\rm{/, "\\mathrm{");
+    }
+    return exp;
 }
 
-function main() {
+function convert(expression) {
+    try {
+        var tree = katex.__parse(preprocess(expression), {});
+    } catch (e) {
+        console.warn(`Warning: Failed to parse: ${e}, skipping.`);
+        return "";
+    }
+    var typ_expression = build_expression(tree);
+
+    return typ_expression;
+
+
+    /* post process
+    for (var i = 0; i < 300; ++i) {
+        norm_str = norm_str.replace('SSSSSS', '$');
+        norm_str = norm_str.replace(' S S S S S S', '$');
+    }
+    console.log(norm_str.replace(/\\label { .*? }/, ""));
+    */
+}
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+
+async function main() {
     const __filename = fileURLToPath(import.meta.url);
     const __dirname = path.dirname(__filename);
 
@@ -25,10 +73,21 @@ function main() {
             return;
         }
 
-        const latexString = data;
+        const lines = data.split('\n');
+        var index = 0;
 
-        console.log('formula: ', latexString);
-        console.log(convert(latexString));
+        rl.on('line', () => {
+            if (index < lines.length) {
+                var latexString = lines[index]
+                console.log(`formula [${index}]:`, latexString);
+                console.log(convert(latexString));
+    
+                index++;
+                if (index === lines.length) {
+                    rl.close();
+                }
+            }
+        });
     });
 }
 
