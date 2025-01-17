529a36294ae002df80b37647dfbc60eb57cdcfe0
ParaN3xus <paran3xus007@gmail.com>
2025-01-17 23:35:13 +0800

    refactor: split index.js

--- index.js
+++ index.js
@@ -1,61 +1,9 @@
-import katex from 'katex';
 import fs from 'fs';
 import path from 'path';
 import readline from 'readline';
-import { build_expression } from './build.js';
-import { fileURLToPath } from 'url';
 import { parse } from 'csv-parse';
 import { stringify } from 'csv-stringify';
-
-// https://github.com/harvardnlp/im2markup/blob/master/scripts/preprocessing/preprocess_latex.js
-function preprocess(exp) {
-    if (exp[0] == "%") {
-        exp = exp.substr(1, exp.length - 1);
-    }
-    exp = exp.split('%')[0];
-
-    exp = exp.split('\\~').join(' ');
-
-    while (exp.indexOf("\\>") !== -1 || exp.indexOf("$") !== -1 || /\\label{.*?}/.test(exp)) {
-        exp = exp.replace(/\\>/, " ");
-        exp = exp.replace('$', ' ');
-        exp = exp.replace(/\\label{.*?}/, "");
-    }
-
-    if (exp.indexOf("matrix") == -1 && exp.indexOf("cases") == -1 &&
-        exp.indexOf("array") == -1 && exp.indexOf("begin") == -1) {
-        while (exp.indexOf("\\\\") !== -1) {
-            exp = exp.replace(/\\\\/, "\\,");
-        }
-    }
-
-    while (exp.indexOf("{\\rm") !== -1 || exp.indexOf("{ \\rm") !== -1 || exp.indexOf("\\rm{") !== -1) {
-        exp = exp.replace(/{\\rm/, "\\mathrm{");
-        exp = exp.replace(/{ \\rm/, "\\mathrm{");
-        exp = exp.replace(/\\rm{/, "\\mathrm{");
-    }
-    return exp;
-}
-
-function convert(expression) {
-    try {
-        var tree = katex.__parse(preprocess(expression), { displayMode: true });
-    } catch (e) {
-        console.warn(`Warning: Failed to parse: ${e}, skipping.`);
-        return "";
-    }
-    var typ_expression = build_expression(tree);
-
-    return typ_expression;
-
-    /* post process
-    for (var i = 0; i < 300; ++i) {
-        norm_str = norm_str.replace('SSSSSS', '$');
-        norm_str = norm_str.replace(' S S S S S S', '$');
-    }
-    console.log(norm_str.replace(/\\label { .*? }/, ""));
-    */
-}
+import convert from "./lib.js"
 
 const rl = readline.createInterface({
     input: process.stdin,
---
+++ lib.js
@@ -0,0 +1,52 @@
+import katex from 'katex';
+import { build_expression } from './build.js';
+
+// https://github.com/harvardnlp/im2markup/blob/master/scripts/preprocessing/preprocess_latex.js
+function preprocess(exp) {
+    if (exp[0] == "%") {
+        exp = exp.substr(1, exp.length - 1);
+    }
+    exp = exp.split('%')[0];
+
+    exp = exp.split('\\~').join(' ');
+
+    while (exp.indexOf("\\>") !== -1 || exp.indexOf("$") !== -1 || /\\label{.*?}/.test(exp)) {
+        exp = exp.replace(/\\>/, " ");
+        exp = exp.replace('$', ' ');
+        exp = exp.replace(/\\label{.*?}/, "");
+    }
+
+    if (exp.indexOf("matrix") == -1 && exp.indexOf("cases") == -1 &&
+        exp.indexOf("array") == -1 && exp.indexOf("begin") == -1) {
+        while (exp.indexOf("\\\\") !== -1) {
+            exp = exp.replace(/\\\\/, "\\,");
+        }
+    }
+
+    while (exp.indexOf("{\\rm") !== -1 || exp.indexOf("{ \\rm") !== -1 || exp.indexOf("\\rm{") !== -1) {
+        exp = exp.replace(/{\\rm/, "\\mathrm{");
+        exp = exp.replace(/{ \\rm/, "\\mathrm{");
+        exp = exp.replace(/\\rm{/, "\\mathrm{");
+    }
+    return exp;
+}
+
+export function convert(expression) {
+    try {
+        var tree = katex.__parse(preprocess(expression), { displayMode: true });
+    } catch (e) {
+        console.warn(`Warning: Failed to parse: ${e}, skipping.`);
+        return "";
+    }
+    var typ_expression = build_expression(tree);
+
+    return typ_expression;
+
+    /* post process
+    for (var i = 0; i < 300; ++i) {
+        norm_str = norm_str.replace('SSSSSS', '$');
+        norm_str = norm_str.replace(' S S S S S S', '$');
+    }
+    console.log(norm_str.replace(/\\label { .*? }/, ""));
+    */
+}
