75bf48a8623922856e4fc25a7f96275dd5d7a8bd
ParaN3xus <paran3xus007@gmail.com>
2025-01-17 23:43:47 +0800

    feat: webpack

---
+++ prod.config.js
@@ -0,0 +1,13 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+export default {
+    entry: './lib.js',
+    output: {
+        filename: 'tex2typ.js',
+        library: 'tex2typ',
+        libraryTarget: 'window'
+    },
+    mode: "development"
+};
